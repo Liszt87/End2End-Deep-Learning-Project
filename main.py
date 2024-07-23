43230abef998d6a417137b1945e4285d65c90607
Liszt87 <dimaspurnomo610@yahoo.com>
2024-07-23 11:53:20 +0700

    training model pipeline

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.cnnClassifier import logger 
 from cnnClassifier.pipeline.stage_data_ingestion_01 import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_prepare_base_model_02 import PrepareBaseModelTrainingPipeline 
+from cnnClassifier.pipeline.stage_model_training_03 import ModelTrainingPipeline                                                       
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -10,3 +12,25 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training Model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
